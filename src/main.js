d3e2877dcd05ffc2906663c8babc45768d555a99
jingcqu <jingchun.qu.work@gmail.com>
2020-10-31 18:12:54 -0400

    Added functionality to buttons
    place bricks,
    place char, place evil,
    and view pathing

--- src/main.js
+++ src/main.js
@@ -14,9 +14,10 @@ var viewPath = false;
 var playerPlaced = false;
 var sourcePlaced = false;
 var evil = {
-        set: function(x, y) {
+        place: function(x, y) {
             this.x = x
             this.y = y
+            sourcePlaced = true
         }
     }
     /** ---------Graph-------------- */
@@ -37,6 +38,9 @@ class Node {
     makeUnavailable() {
         this.available = false;
     }
+    makeIsLight() {
+        this.isLight = true;
+    }
     setLeft(lNeigh) {
         this.left = lNeigh;
     }
@@ -49,32 +53,128 @@ class Node {
     setBot(bNeigh) {
         this.bottom = bNeigh;
     }
+    isAvailable() {
+        return this.available;
+    }
+    turnOnSource() {
+        this.isSource = true;
+    }
+    turnOffSource() {
+        this.isSource = false;
+    }
+    turnOnTarget() {
+        this.isTarget = true;
+    }
+    turnOffTarget() {
+        this.isTarget = false;
+    }
+}
+
+function posToCol(x, y) {
+    return (y * Math.floor(boardWidth / cellLength)) + x
+}
+
+function colToPos(index) {
+    return {
+        x: index % Math.floor(boardWidth / cellLength),
+        y: Math.floor(index / Math.floor(boardWidth / cellLength))
+    };
 }
 
 var boardGraph = {
     make: function(width, height, lightList) {
+        //console.log("enter make board graph")
         this.collection = [];
+        //console.log(width)
+        //console.log(height)
         for (var iX = 0; iX < width; iX += cellLength) {
             for (iY = 0; iY < height; iY += cellLength) {
-                var cCell = new Node(iX, iY)
-                this.collection.push(cCell)
+                var cCell = new Node(iX, iY);
+                this.collection.push(cCell);
             }
         }
-        maxX = Math.floor(width / cellLength)
-        maxY = Math.floor(width / cellLength)
+        maxX = Math.floor(width / cellLength);
+        maxY = Math.floor(width / cellLength);
+        //console.log(this.collection.length)
         for (var i = 0; i < this.collection.length; i++) {
             let x = i % maxX;
-            let y = Math.floor(i / maxX)
-
+            let y = Math.floor(i / maxX);
+            let currInd = posToCol(x, y);
+            /** connect board -------------------------------------------------------------- */
+            if ((y - 1) >= 0) {
+                let ind = posToCol(x, y - 1);
+                //let pos = colToPos(ind)
+                //console.log(x, y, ind, x, pos.y)
+                this.collection[currInd].setTop(this.collection[ind]);
+                this.collection[ind].setBot(this.collection[currInd]);
+            }
+            if ((x - 1) >= 0) {
+                let ind = posToCol(x - 1, y);
+                this.collection[currInd].setLeft(this.collection[ind]);
+                this.collection[ind].setRight(this.collection[currInd]);
+            }
         }
     },
     checkCell: function(x, y) { //false - unavailable
-        if ((x < 0) || (y < 0) || (x >= boardWidth) || (y >= boardHeight)) {
-            return false
+        if ((x < 0) || (y < 0) || (x >= Math.floor(boardWidth / cellLength)) || (y >= Math.floor(boardHeight / cellLength))) {
+            return false;
         } else {
-            let index = Math.floor(y / cellLength) * Math.floor(boardWidth / cellLength)
-            index += Math.floor(x / cellLength)
+            /**check cell if available------------------------------------ */
+            index = posToCol(x, y);
+            return this.collection[index].isAvailable();
+        }
+    },
+    setLights: function(lset) {
+        //console.log(this.collection.length);
+        //console.log(this.collection[0]);
+        for (var i = 0; i < lset.length; i++) {
+            x = lset[i].getX();
+            y = lset[i].getY();
+            index = posToCol(x, y);
+            //console.log(x);
+            //console.log(y);
+            //console.log(index);
+            this.collection[index].makeUnavailable();
+            this.collection[index].makeIsLight();
+        }
+    },
+    dijkstra: function() {
 
+    },
+    astar: function() {
+
+    },
+    bfs: function() {
+
+    },
+    grassfire: function() {
+
+    },
+    dfs: function() {
+
+    },
+    pathing: function(strategy) {
+        switch (strategy) {
+            case "dijkstra":
+                console.log("dijkstra")
+                this.dijkstra()
+                break;
+            case "astar":
+                console.log("astar")
+                this.astar()
+                break;
+            case "bfs":
+                console.log("bfs")
+                this.bfs()
+                break;
+            case "grassFire":
+                console.log("grassfire")
+                this.grassfire()
+                break;
+            case "dfs":
+                console.log("dfs")
+                this.dfs()
+                break;
         }
     }
 }
@@ -83,20 +183,21 @@ var boardGraph = {
 
 var player = {
     place: function(x, y) {
-        this.x = x
-        this.y = y
+        this.x = x;
+        this.y = y;
+        playerPlaced = true;
     },
     moveL: function() {
-        this.x -= cellLength
+        this.x -= cellLength;
     },
     moveR: function() {
-        this.x += cellLength
+        this.x += cellLength;
     },
     moveU: function() {
-        this.y -= cellLength
+        this.y -= cellLength;
     },
     moveD: function() {
-        this.y += cellLength
+        this.y += cellLength;
     }
 }
 
@@ -108,14 +209,29 @@ var myCanvas = {
         this.canvas.height = 500;
         boardWidth = this.canvas.width
         boardHeight = this.canvas.height
-        this.canvas.click
         this.context = this.canvas.getContext("2d");
-        context = this.canvas.addEventListener("mousedown", place)
+        this.mouseDown = false;
+        this.canvas.addEventListener("mousedown", function() { this.mouseDown = true })
+        this.canvas.addEventListener("mousedown", place)
+        this.canvas.addEventListener("mousemove", function(event) {
+            if (this.mouseDown) {
+                place(event)
+            }
+        })
+        this.canvas.addEventListener("mouseup", function() {
+            console.log("mouseDown")
+            this.mouseDown = false
+        })
+
 
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 
-        setLights(this.canvas.width, this.canvas.height);
+        lset = setLights(this.canvas.width, this.canvas.height);
         drawGrid(this.canvas.width, this.canvas.height);
+        //console.log("exited drawGrid")
+        boardGraph.make(boardWidth, boardHeight)
+        boardGraph.setLights(lset)
+            /** --------set light cells to be unavailable-------------------------- */
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -129,6 +245,10 @@ function place(event) {
     mouseX = mouseX - (mouseX % cellLength);
     mouseY = mouseY - (mouseY % cellLength);
     var ctx = myCanvas.canvas.getContext("2d");
+    index = posToCol(mouseX / cellLength, mouseY / cellLength)
+    if ((boardGraph.collection[index].isAvailable() === false) || (boardGraph.collection[index].isTarget === true) || (boardGraph.collection[index].isSource === true)) {
+        return
+    }
     /**
      * var brickBool = false;
     var placeChar = false;
@@ -142,18 +262,28 @@ function place(event) {
             return;
         }
         playerPlaced = true;
+        boardGraph.collection[index].turnOnTarget()
+        player.place(mouseX / cellLength, mouseY / cellLength)
     } else if (placeEvil) {
         ctx.fillStyle = "red";
         if (sourcePlaced) {
             return;
         }
         sourcePlaced = true;
+        boardGraph.collection[index].turnOnSource()
+        evil.place(mouseX / cellLength, mouseY / cellLength)
     } else if (brickBool) {
         ctx.fillStyle = "brown";
+        boardGraph.collection[index].makeUnavailable();
     } else if ((placeChar === false) && (placeEvil === false) && (brickBool === false)) {
+        //temp for testing
+        console.log(boardGraph.collection[index].isAvailable())
+
         return;
     }
     ctx.fillRect(mouseX, mouseY, cellLength, cellLength)
+
+
 }
 
 function makeBoard() {
@@ -197,11 +327,11 @@ function setLights(width, height) {
         y = Math.floor(rn / Math.floor(width / cellLength));
         x *= cellLength;
         y *= cellLength;
-        cl = new Light(x, y);
+        cl = new Light(x / cellLength, y / cellLength);
         var isRepeat = false
         for (var i = 0; i < lset.length; i++) {
             if (comparePos(cl, lset[i])) {
-                console.log(cl.x);
+                //console.log(cl.x);
                 isRepeat = true;
                 break;
             }
@@ -212,26 +342,26 @@ function setLights(width, height) {
         }
         lset.push(cl);
     }
-    console.log("exitloop");
+    //console.log("exitloop");
     var c = document.getElementsByTagName("canvas")[0];
-    console.log(c);
+    //console.log(c);
     ctx = c.getContext("2d");
 
     for (var i = 0; i < lset.length; i++) {
         let img = new Image();
         img.className = String(i);
         img.src = "/assets/meLight.png";
-        console.log(img);
-        let x = lset[i].getX();
-        let y = lset[i].getY();
-        console.log(x);
-        console.log(y);
+        //console.log(img);
+        let x = lset[i].getX() * cellLength;
+        let y = lset[i].getY() * cellLength;
+        //console.log(x);
+        //console.log(y);
         img.onload = function() {
                 ctx.drawImage(img, x, y);
-
             }
             //    console.log("drawing")
     }
+    return lset
 }
 
 function drawGrid(width, height) {
@@ -255,7 +385,7 @@ function placeBrickButton() {
     placeEvil = false;
     startButt = false;
     viewPath = false;
-    console.log("brick clicked")
+    //console.log("brick clicked")
 }
 
 function placeCharButton() {
@@ -264,7 +394,7 @@ function placeCharButton() {
     placeEvil = false;
     startButt = false;
     viewPath = false;
-    console.log("char clicked")
+    //console.log("char clicked")
 }
 
 function placeEvilButton() {
@@ -273,7 +403,7 @@ function placeEvilButton() {
     placeEvil = true;
     startButt = false;
     viewPath = false;
-    console.log("evil clicked")
+    //console.log("evil clicked")
 }
 
 function startButton() { //place holder
@@ -282,7 +412,10 @@ function startButton() { //place holder
     placeEvil = false;
     startButt = true;
     viewPath = false;
-    console.log("start clicked")
+    //console.log("start clicked")
+    if ((playerPlaced === false) || (sourcePlaced === false)) {
+        return;
+    }
 }
 
 function viewPathButton() {
@@ -292,4 +425,11 @@ function viewPathButton() {
     startButt = false;
     viewPath = true;
     console.log("view path clicked")
+
+    if ((playerPlaced === false) || (sourcePlaced === false)) {
+        return;
+    }
+    strategy = document.getElementById("strategy").value
+        //console.log(document.getElementById("strategy"))
+    boardGraph.pathing(strategy)
 }
